413ab156ea20ae49a5338e624eb2dfb3dc1a5230
shka <45268944+shka86@users.noreply.github.com>
2022-03-19 00:41:22 +0900

    add tab and prepare to add another apps

--- guipandoc.py
+++ guipandoc.py
@@ -6,6 +6,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from pathlib import Path as p
 
+# from matplotlib import container
+
 import md2html
 
 from input_list import *
@@ -26,12 +28,32 @@ def guipandoc(root):
     btn_convert.pack(ipadx=10, ipady=10, padx=15, pady=15)
 
 
+def button(root):
+    btn_convert = tk.Button(master=root, text='btn', command=lambda: print("hello!!"))
+    btn_convert.pack(ipadx=10, ipady=10, padx=15, pady=15)
+
+
 if __name__ == "__main__":
+
+    # main window
     root = tk.Tk()
     root.title("guipandoc")
     root.geometry("500x250")
+    nb = ttk.Notebook(root)
+    nb.pack(expand=1, fill='both')
 
-    guipandoc(root)
+    # tabs
+    tab_guipandoc = tk.Frame(nb)
+    nb.add(tab_guipandoc, text='pandoc')
+    guipandoc(tab_guipandoc)
+
+    tab_button = tk.Frame(nb)
+    nb.add(tab_button, text='btn')
+    button(tab_button)
+
+
+
+
 
     # Start App
     root.mainloop()
