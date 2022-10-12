e60c289b1241c974feba6523098d5ac6ef4b1f4a
shka <45268944+shka86@users.noreply.github.com>
2022-10-13 00:40:05 +0900

    class化して拡張しやすくしただけ

--- cpan.py
+++ cpan.py
@@ -8,7 +8,6 @@ import json
 
 import md2html
 
-
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tgt', type=str,
@@ -20,73 +19,91 @@ def argparser():
     args = parser.parse_args()
     return args
 
+class Cpan():
+    def __init__(self, args) -> None:
+        self.args = args
+        self.histories = []  # 過去の変換履歴
+        self.markdowns = []  # 変換したmarkdown
+        self.htmls = []  # 生成したhtml
 
-def conv_a_file(tgt):
-    md2html.main(tgt)
-    newhistory = str(tgt)
-    return newhistory
+        self._init()
 
+    def _init(self):
+        history = "./history.json"
+        if p(history).is_file():
+            with open(history, "r", encoding="utf-8") as f:
+                histories = json.load(f)['histories']
+        else:
+            histories = []
 
-def conv_a_dir(tgt):
-    tgts = list(p(tgt).glob('**/*.md'))
-    for i, x in enumerate(tgts):
-        print(f'{i}: {x}')
+        self.histories = histories
 
-    tgtnum = input('Select a file number. If all, enter "all": ')
+        # -----------------------------------
+        if args.list:
+            args.tgt = self.select_from_history(histories)
 
-    if tgtnum == "all":
+        if args.tgt:
+            tgt = args.tgt
+
+            if p(tgt).is_dir():  # If dir, show list and receive which number to convert.
+                newhistory = self.conv_a_dir(tgt)
+            elif p(tgt).is_file():  # If file, convert to html. Expecting markdown.
+                newhistory = self.conv_a_file(tgt)
+
+        # -----------------------------------
+        if newhistory:
+            newhistory = str(p(newhistory).as_posix())
+            newhistories = [newhistory]
+            for x in histories:
+                x = str(p(x).as_posix())
+                if x != newhistory:
+                    newhistories.append(x)
+
+            with open(history, "w", encoding="utf-8") as f:
+                jsondata = {'histories': newhistories}
+                json.dump(jsondata, f, indent=4, ensure_ascii=False)
+
+    def select_from_history(self, histories):
+        for i, x in enumerate(histories):
+            print(f'{i}: {x}')
+        tgtnum = input('Select a number: ')
+        return str(histories[int(tgtnum)])
+
+    def conv_a_dir(self, tgt):
+        tgts = list(p(tgt).glob('**/*.md'))
+        for i, x in enumerate(tgts):
+            print(f'{i}: {x}')
+
+        tgtnum = input('Select a file number. If all, enter "all": ')
+
+        if tgtnum == "all":
+            newhistory = str(tgt)
+            for x in tgts:
+                m2h = md2html.main(x)
+                self.update_info(m2h)
+            return newhistory
+
+        else:
+            tgt = tgts[int(tgtnum)]
+            m2h = md2html.main(tgt)
+            self.update_info(m2h)
+            newhistory = str(tgt)
+            return newhistory
+
+    def conv_a_file(self, tgt):
+        m2h = md2html.main(tgt)
+        self.update_info(m2h)
         newhistory = str(tgt)
-        for x in tgts:
-            md2html.main(x)
         return newhistory
 
-    else:
-        tgt = tgts[int(tgtnum)]
-        md2html.main(tgt)
-        newhistory = str(tgt)
-        return newhistory
-
-
-def select_from_history(histories):
-    for i, x in enumerate(histories):
-        print(f'{i}: {x}')
-    tgtnum = input('Select a number: ')
-    return str(histories[int(tgtnum)])
+    def update_info(self, m2h):
+        self.markdowns.append(m2h.path_markdown)
+        self.htmls.append(m2h.path_html)
 
 # -----------------------------------
 if __name__ == "__main__":
 
     args = argparser()
-
-    history = "./history.json"
-    if p(history).is_file():
-        with open(history, "r", encoding="utf-8") as f:
-            histories = json.load(f)['histories']
-    else:
-        histories = []
-
-    # -----------------------------------
-    if args.list:
-        args.tgt = select_from_history(histories)
-
-    if args.tgt:
-        tgt = args.tgt
-
-        if p(tgt).is_dir():  # If dir, show list and receive which number to convert.
-            newhistory = conv_a_dir(tgt)
-        elif p(tgt).is_file():  # If file, convert to html. Expecting markdown.
-            newhistory = conv_a_file(tgt)
-
-
-    # -----------------------------------
-    if newhistory:
-        newhistory = str(p(newhistory).as_posix())
-        newhistories = [newhistory]
-        for x in histories:
-            x = str(p(x).as_posix())
-            if x != newhistory:
-                newhistories.append(x)
-
-        with open(history, "w", encoding="utf-8") as f:
-            jsondata = {'histories': newhistories}
-            json.dump(jsondata, f, indent=4, ensure_ascii=False)
+    cpan = Cpan(args)
+    print(cpan.markdowns)
+    print(cpan.htmls)
--- md2html.py
+++ md2html.py
@@ -15,42 +15,54 @@ import tempfile
     TemporaryDirectory で作業場所を作って実行する。
 """
 
+class Md2Html():
+    def __init__(self, markdown) -> None:
+        self.path_markdown = p(markdown).absolute().as_posix()
+        self.path_html = None
+
+        self.generate_html(self.path_markdown)
+
+    def generate_html(self, tgt):
+        tgt = p(tgt).resolve()
+        print("---")
+        print(f"convert tgt: \n{tgt}")
+        print("---")
+        if (not tgt.is_file()) or (str(tgt) == "."):
+            return 0
+
+        pagetitle = f"{tgt.stem}"
+        outfile = p(f"{tgt.parent}/{tgt.stem}.html")
+        cwd = p.cwd()
+
+        with tempfile.TemporaryDirectory() as td:
+            # pandoc実行環境のコピー(UNCパス禁止への対応)
+            tmp_wd = p(td) / cwd.stem
+            shutil.copytree(cwd, tmp_wd)
+
+            # 変換対象ファイルのコピー
+            shutil.copytree(tgt.parent, tmp_wd / p("work"))
+            items = tmp_wd.glob("**/*")
+            for item in items:
+                item.chmod(0o777)
+            os.chdir(tmp_wd / p("work"))
+
+            css = p(tmp_wd) / p("style/test.css")
+            template = p(tmp_wd) / p("style/test.html")
+            cmd = f'pandoc {tgt} -o {outfile} -s --self-contained -c {css} --metadata pagetitle="{pagetitle}" --toc --toc-depth=3 --template={template} -t html5'
+
+            subprocess.run(cmd.split(' '))
+
+            os.chdir(cwd)
+
+        print(f"Done!! output: \n{outfile}")
+        self.path_html = p(outfile).absolute().as_posix()
 
 def main(tgt):
-    tgt = p(tgt).resolve()
-    print("---")
-    print(f"convert tgt: \n{tgt}")
-    print("---")
-    if (not tgt.is_file()) or (str(tgt) == "."):
-        return 0
-
-    pagetitle = f"{tgt.stem}"
-    outfile = p(f"{tgt.parent}/{tgt.stem}.html")
-    cwd = p.cwd()
-
-    with tempfile.TemporaryDirectory() as td:
-        # pandoc実行環境のコピー(UNCパス禁止への対応)
-        tmp_wd = p(td) / cwd.stem
-        shutil.copytree(cwd, tmp_wd)
-
-        # 変換対象ファイルのコピー
-        shutil.copytree(tgt.parent, tmp_wd / p("work"))
-        items = tmp_wd.glob("**/*")
-        for item in items:
-            item.chmod(0o777)
-        os.chdir(tmp_wd / p("work"))
-
-        css = p(tmp_wd) / p("style/test.css")
-        template = p(tmp_wd) / p("style/test.html")
-        cmd = f'pandoc {tgt} -o {outfile} -s --self-contained -c {css} --metadata pagetitle="{pagetitle}" --toc --toc-depth=3 --template={template} -t html5'
-
-        subprocess.run(cmd.split(' '))
-
-        os.chdir(cwd)
-
-    print(f"Done!! output: \n{outfile}")
+    m2h = Md2Html(tgt)
+    return m2h
 
 if __name__ == '__main__':
     args = sys.argv
     tgt = p(args[1])
+
     main(tgt)
